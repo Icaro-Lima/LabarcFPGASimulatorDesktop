ff2efd1e07a2e4078a34fd5381ad37ec0acd152d
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-06-06 11:46:06 -0300

    coloca a janela para iniciar sempre no centro da tela

--- gui.cpp
+++ gui.cpp
@@ -78,9 +78,11 @@ const char *mono_fonts[] = { "Lucida Console",
                              "" };
 
 void init_gui(int argc, char** argv) {
-  window = new Fl_Window(600, 360, "Labarc FPGA Simulator");
+  int window_width = 600;
+  int window_height = 360;
+  window = new Fl_Window(Fl::w() / 2 - window_width / 2, Fl::h() / 2 - window_height / 2, window_width, window_height, "Labarc FPGA Simulator");
 
-  swi = new SWI_Buttons(30,10,30,17,30);
+  swi = new SWI_Buttons(30, 10, 30, 17, 30);
   
   leds = new LEDs(55, 40, 30);
 
