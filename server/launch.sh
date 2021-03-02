bc1fbd14e729717c6376354e801b0a1d1f0b529e
emelcher <elmar@dsc.ufcg.edu.br>
2021-03-02 08:22:23 -0300

    launch processing according to FPGAs connected

--- server/launch.sh
+++ server/launch.sh
@@ -1,6 +1,6 @@
-altera
-./launch 0 &
-disown -h %1
-./launch 1 &
-disown -h %1
-logout
+source /usr/local/altera/altera.sh
+n=$(jtagconfig | grep ^.\) | tail -1 | cut -d\) -f1)
+for i in $(seq 0 $(expr $n - 1)); do
+  ./launch $i &
+  disown -h %1
+done
