ad7e13cca1aa565bc856af3b6c958e3fce520782
emelcher <elmar@dsc.ufcg.edu.br>
2021-04-21 16:07:51 -0300

    adaptacoes para poder usar com spike

--- server/client.php
+++ server/client.php
@@ -20,14 +20,18 @@
   }
 
   if( isset($_GET["name"]) && isset($_GET["port"]) && isset($_GET["data"]) &&
-      !empty($_GET["name"]) && !empty($_GET["data"]) &&
+      !empty($_GET["name"]) && isset($_GET["data"]) &&
       ( !preg_match('/[^01]/', $_GET["data"]) || !strcmp($_GET["data"], "exit") 
         || !preg_match('/[^fhmpqrsuvw]/', $_GET["data"][0]) 
       ) ) {
         // data must contain only 0 and 1  or "exit"
     $host    = $_GET["name"];
     $port    = $_GET["port"];
-    $message = substr($_GET["data"],0,8) . " " . get_client_ip() . "\n";
+    if(isset($_GET["m"])) {
+       $message = $_GET["data"] . "\n";
+    } else {
+       $message = substr($_GET["data"],0,8) . " " . get_client_ip() . "\n";
+    }
     // Error messages must start with letter 'S'
     // create socket
     $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Socket creation failed\n");
@@ -36,8 +40,11 @@
     // send string to server
     socket_write($socket, $message, strlen($message)) or die("Sending data failed\n");
     // get server response
-    $result = socket_read ($socket, 80, PHP_NORMAL_READ) or die("Server response failed\n");
+    $result = socket_read ($socket, 100, PHP_NORMAL_READ) or die("Server response failed\n");
     echo $result;
+    while(isset($_GET["m"]) && ($result = socket_read ($socket, 80, PHP_NORMAL_READ))) {
+       echo "\n<br>" . preg_replace('/>/', '&#62', preg_replace('/</', '&#60', $result));
+    }
     // close socket
     socket_close($socket);
   }
